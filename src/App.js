9071db9bdcca49f1836d588e9c8db5f9ba1c42f6
vk3796565@gmail.com <vk3796565@gmail.com>
2022-03-30 02:06:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Home from './pages/home/Home';
 import Topbar from './topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
      <Topbar />
+     <Home/>
     </div>
   );
 }
