7692f5cad3f1df245ba563fbecba06e67b44f3c8
vk3796565@gmail.com <vk3796565@gmail.com>
2022-03-30 17:32:10 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-
-import './App.css';
-import Home from './pages/home/Home';
-import Topbar from './components/topbar/Topbar';
-import Single from './pages/single/Single';
-import Write from './pages/write/Write';
+import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const currentUser = false;
   return (
-    <div className="App">
-     <Topbar />
-     {/* <Home/> */}
-     {/* <Single/> */}
-     <Write/>
-    </div>
+    <Router>
+      <Topbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/posts">
+          <Home />
+        </Route>
+        <Route path="/register">
+          {currentUser ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{currentUser ? <Home /> : <Login />}</Route>
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+        <Route path="/settings">
+          {currentUser ? <Settings /> : <Login />}
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,26 +1,58 @@
 import React from 'react'
 import "./topbar.css"
-import  pimg from "./img1.jpg";
+import pimg from "./img1.jpg";
+import {Link } from "react-router-dom";
+
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function Topbar() {
+  const user = false;
   return (
     <div className="Top">
-         <div className="topLeft">Mr.darkcoder</div>
-         <div className="topCenter">
-             <ul className="topList">
-                 <li className="topListItem">Home</li>
-                 <li className="topListItem">About</li>
-                 <li className="topListItem">CONTACT</li>
-                 <li className="topListItem">WRITE</li>
-                 <li className="topListItem">LOGOUT</li>
-             </ul>
-         </div>
-         <div className="topRight">
-             <img className="topImg" src={pimg } alt="hello" srcset="" />
-             <SearchIcon className="searchicon"/>
-         </div>
-         
+      <div className="topLeft">Mr.darkcoder</div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">
+          <Link className="link" to="/write">
+              WRITE
+            </Link></li>
+            {user && <li className="topListItem">LOGOUT</li>}
+          
+        </ul>
+      </div>
+      <div className="topRight">
+      {user ? (
+          <Link className="link" to="/settings">
+            <img
+              className="topImg"
+              src={pimg}
+              alt=""
+            />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+        <SearchIcon className="searchicon" />
+      </div>
+
     </div>
   )
 }
